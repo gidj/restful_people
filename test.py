1357615c192f35f2f942643a71cae453734f43da
Gideon VanRiette <gvanriette@gmail.com>
2019-07-23 22:13:08 -0500

    Duplicate for other test cases

--- test.py
+++ test.py
@@ -22,3 +22,25 @@ class TestLineParser(unittest.TestCase):
         assert person.gender == self.GENDER
         assert person.favorite_color == self.FAVORITE_COLOR
         assert person.date_of_birth == self.DATE_OF_BIRTH
+
+    def test_space_line(self):
+        test_line = " ".join(self.TEST)
+        line_parser = LineParser.parser_with_delimiter(" ")
+        person = line_parser.parse_line(test_line)
+
+        assert person.first_name == self.FIRST_NAME
+        assert person.last_name == self.LAST_NAME
+        assert person.gender == self.GENDER
+        assert person.favorite_color == self.FAVORITE_COLOR
+        assert person.date_of_birth == self.DATE_OF_BIRTH
+
+    def test_pipe_line(self):
+        test_line = "|".join(self.TEST)
+        line_parser = LineParser.parser_with_delimiter("|")
+        person = line_parser.parse_line(test_line)
+
+        assert person.first_name == self.FIRST_NAME
+        assert person.last_name == self.LAST_NAME
+        assert person.gender == self.GENDER
+        assert person.favorite_color == self.FAVORITE_COLOR
+        assert person.date_of_birth == self.DATE_OF_BIRTH
