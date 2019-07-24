6fa24afafdd05dceac84c1b57843844c015acefe
Gideon VanRiette <gvanriette@gmail.com>
2019-07-23 22:11:36 -0500

    Add unit test

---
+++ test.py
@@ -0,0 +1,24 @@
+import unittest
+
+from util.parser import FileParser, LineParser
+
+
+class TestLineParser(unittest.TestCase):
+    LAST_NAME = "LastName"
+    FIRST_NAME = "FirstName"
+    GENDER = "Gender"
+    FAVORITE_COLOR = "FavoriteColor"
+    DATE_OF_BIRTH = "DateOfBirth"
+
+    TEST = (LAST_NAME, FIRST_NAME, GENDER, FAVORITE_COLOR, DATE_OF_BIRTH)
+
+    def test_commma_line(self):
+        test_line = ",".join(self.TEST)
+        line_parser = LineParser.parser_with_delimiter(",")
+        person = line_parser.parse_line(test_line)
+
+        assert person.first_name == self.FIRST_NAME
+        assert person.last_name == self.LAST_NAME
+        assert person.gender == self.GENDER
+        assert person.favorite_color == self.FAVORITE_COLOR
+        assert person.date_of_birth == self.DATE_OF_BIRTH
