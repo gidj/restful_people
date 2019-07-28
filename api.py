d5b6571ceaabd76877dfbe301d12037b53fcb60f
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-07-28 17:43:58 -0500

    Very basic api, defining our routes

--- api.py
+++ api.py
@@ -4,3 +4,13 @@ app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
+
+
+@app.route("/records", methods=["POST"])
+def records():
+    return "Posting to records"
+
+
+@app.route("/records/<string:sort_key>", methods=["GET"])
+def records_sorted(sort_key: str):
+    return "Getting records, sorted by '{}'".format(sort_key)
