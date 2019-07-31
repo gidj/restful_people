5dae7bb5fe072b1828829c7f61094e9145690a5d
Gideon VanRiette <Gideon.VanRiette@vividseats.com>
2019-07-31 13:53:55 -0500

    Add additional functions to complete functionality

--- util/functional_parser.py
+++ util/functional_parser.py
@@ -1,21 +1,23 @@
-from typing import TextIO, Tuple
+from typing import Callable, TextIO, Tuple
 
 from storage.models import Person
 
 
-def file_to_person(file: TextIO) -> Tuple[Person]:
+def file_to_people(file: TextIO) -> Tuple[Person]:
     lines = _file_lines(file)
-    pass
+    parsed_lines = map(_parse_line, lines)
+    return tuple(map(Person._make, parsed_lines))
 
 
-def _file_lines(file: TextIO) -> Tuple:
+def _file_lines(file: TextIO) -> Tuple[str]:
     file.seek(0)
     return tuple(file.read().splitlines())
 
 
-def _parser_from_delimiter(delimiter: str):
+def _parser_from_delimiter(delimiter: str) -> Callable[[str], Tuple[str]]:
     def f(line: str) -> Tuple[str]:
         return tuple(line.split(delimiter))
+
     return f
 
 
@@ -26,3 +28,9 @@ def _delimiter_from_line(line: str) -> str:
     elif "|" in line:
         delimiter = "|"
     return delimiter
+
+
+def _parse_line(line: str) -> Tuple[str]:
+    delimiter = _delimiter_from_line(line)
+    parser = _parser_from_delimiter(delimiter)
+    return parser(line)
